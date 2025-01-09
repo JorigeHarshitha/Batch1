4eeb31fd3f39890d853652cf64f87f20b78778ad
JorigeHarshitha <2211cs010239@mallareddyuniversity.ac.in>
2025-01-09 15:05:57 +0530

    my new project

---
+++ Day4/fun-expression.js
@@ -0,0 +1,50 @@
+//let demo = function()
+//{
+    //console.log("hello");
+//}
+//console.log(demo);
+//demo()
+
+//let operator = function(a)
+//{
+   // console.log(a);
+    //console.log(arguments);
+    //console.log(arguments[1]);
+    //console.log(arguments[2]);
+//}
+//operator(10 , 20 , 30)
+
+//let demo1 = function()
+//{
+    //let a = Number(prompt("enter a value"));
+    //let b = Number(prompt("enter b value"));
+    //let c = Number(prompt("enter c value"));
+    //let d = a + b + c;
+    //console.log(d);
+    //let e = a - b - c;
+    //console.log(e);
+    //let f = a * b * c;
+    //console.log(f);
+    //let g = a / b / c;
+    //console.log(g);
+    //let h = a | b | c;
+    //console.log(h);
+    //if(a >= b && a >= c)
+    //{
+        //console.log("a is gratest");
+    //}else if(b >= a && b >= c)
+    ////{
+        //console.log("b is gratest");
+    //}else{
+        //console.log("c is gratest");
+    //}
+//}
+//demo1()
+
+function main(a)
+{
+    
+}
+main(function(){
+
+})
